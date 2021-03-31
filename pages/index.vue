d2df07795cf1822e7781c1e4e1175ffff453b222
Bram <bram@owow.io>
2021-03-31 13:55:54 +0200

    Theme configuration for Pilltable

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
     <NavBar />
-    <h1>Pilltable website</h1>
+    <h1 class="font-rubik font-medium text-22xl">Pilltable website</h1>
   </div>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#26254F',
+        'background': '#1D1A3B',
+        'highlight': '#4A57FC',
+        'light-grey': '#D6D6E0',
+        'opacity-elements': 'rgba(255, 255, 255, 0.02)',
+      },
+      fontFamily: {
+        'rubik': ['Rubik', 'sans-serif'],
+      },
+      fontSize: {
+        '22xl': '1.375rem',
+        '40xl': '2.5rem',
+        '56xl': '3.5rem',
+      },
+    },
   },
   variants: {
     extend: {},
