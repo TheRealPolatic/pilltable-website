59a55f592e8b54d389f2c263f41584c8b4147bc7
Bram <bram@owow.io>
2021-03-31 14:39:22 +0200

    Default page styling and margin added

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,7 @@
 <template>
-  <div>
-    <Nuxt />
+  <div class="w-full bg-primary h-screen">
+    <div class="2xl:mx-72 xl:mx-36 lg:mx-16 md:mx-12 mx-6">
+      <Nuxt />
+    </div>
   </div>
 </template>
