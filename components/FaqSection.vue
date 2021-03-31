ebd6209e33dc1527daed1dd6df53e3bf95a252a3
mdw_08 <michelledewit1.mdw@gmail.com>
2021-03-31 17:49:26 +0200

    FAQ section functionality added

--- components/FaqSection.vue
+++ components/FaqSection.vue
@@ -1,18 +1,22 @@
 <template>
   <div class="mt-12 font-rubik">
     <h2 class="text-sm text-highlight tracking-widest uppercase">Questions</h2>
-    <h1 class="text-40xl font-medium text-white">What the FAQ!? 🤔</h1>
+    <h1 class="md:text-40xl text-4xl mt-3 font-medium text-white tracking-wide">
+      What the FAQ!? 🤔
+    </h1>
 
-    <div class="bg-primary my-6 py-6 px-6">
+    <div class="bg-primary mt-10 py-6 px-6 rounded-md">
       <div class="text-lg">
         <!-- Expand/collapse question button -->
         <button
           type="button"
-          class="text-left w-full flex justify-between items-start"
+          class="text-left w-full flex justify-between items-start focus:outline-none"
           aria-controls="faq-0"
           aria-expanded="false"
+          :aria-checked="isOpen"
+          @click="isOpen = !isOpen"
         >
-          <span class="font-medium text-white mb-4">
+          <span class="font-medium text-white">
             What will happen when there is a data leak?
           </span>
           <span class="ml-6 h-7 flex items-center">
@@ -24,7 +28,11 @@
                   Open: "-rotate-180", Closed: "rotate-0"
                 -->
             <svg
-              class="rotate-0 h-6 w-6 transform"
+              :class="
+                isOpen
+                  ? 'rotate-0 h-6 w-6 transform'
+                  : 'rotate-180 h-6 w-6 transform'
+              "
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
               viewBox="0 0 24 24"
@@ -41,7 +49,13 @@
           </span>
         </button>
       </div>
-      <p class="text-base text-white font-thin w-11/12">
+      <p
+        :class="
+          isOpen
+            ? 'hidden text-base text-white font-thin w-11/12 mt-4'
+            : 'text-base text-white font-thin w-11/12 mt-4'
+        "
+      >
         Yes! All the data is encrypted with military grade 🔐 ️encryption. Since
         we don’t collect any personally identifiable information, you are at no
         risk.
@@ -49,3 +63,11 @@
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  data: () => ({
+    isOpen: true,
+  }),
+}
+</script>
