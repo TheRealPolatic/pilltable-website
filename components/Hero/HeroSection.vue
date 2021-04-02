4d3d9d70706cb1aff110fe5cff1d47579dd47e3d
Bram <bram@owow.io>
2021-04-02 23:07:55 +0200

    Added a headline, tagline and mockup

---
+++ components/Hero/HeroSection.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="px-40 text-center min-h-screen my-56">
+    <div class="tagline">Ecstacy tracker</div>
+    <h1>The ecstasy tracking tool you’ll enjoy using</h1>
+    <p>Anonymously create or join a group and keep track of everyone's ecstasy usage with ease.</p>
+    <img class="mt-40" src="~/assets/mockups/hero-mockup.png" alt="" srcset="" />
+  </div>
+</template>
