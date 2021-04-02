1b737c3caede398c5db9f57cb47beccff8294575
TheRealPolatic <d.toirkens@student.fontys.nl>
2021-04-02 16:00:07 +0200

    Added navabr

---
+++ components/NavBar.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="sticky">
+    <div class="mx-auto px-4 sm:px-6">
+      <div class="flex justify-between items-center py-6 md:justify-start md:space-x-10">
+        <div class="flex justify-start lg:w-0 lg:flex-1">
+          <a href="#">
+            <h3>Pilltable</h3>
+          </a>
+        </div>
+
+        <nav class="flex justify-between">
+          <p href="#" class="p-light pr-10">Home</p>
+          <p href="#" class="p-light">Features</p>
+        </nav>
+
+        <div class="md:flex items-center justify-end md:flex-1 lg:w-0">
+          <a
+            href="#"
+            class="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
+          >
+            Try Now
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
