ad7c15748d6f28a43d7cf846b6009a52f873ff17
Bram <bram@owow.io>
2021-04-02 22:47:26 +0200

    Added heading and tagline to the pricing table section

--- components/PricingTable/PricingTableSection.vue
+++ components/PricingTable/PricingTableSection.vue
@@ -1,23 +1,20 @@
 <template>
   <div>
-    <div>
-      <div class="grid grid-cols-12 gap-2 p-6">
-        <div class="col-span-6"></div>
-        <div class="col-span-3 mx-auto">
-          <p class="p-bold">Free</p>
-        </div>
-        <div class="col-span-3 mx-auto">
-          <p class="p-bold">Unlimited</p>
-        </div>
+    <div class="tagline text-center">TRY FOR FREE</div>
+    <h2 class="text-center">Pricing table</h2>
+
+    <div class="grid grid-cols-12 gap-2 p-6">
+      <div class="col-span-6"></div>
+      <div class="col-span-3 mx-auto">
+        <p class="p-bold">Free</p>
+      </div>
+      <div class="col-span-3 mx-auto">
+        <p class="p-bold">Unlimited</p>
       </div>
     </div>
 
     <!-- Render all features -->
-    <PricingTableRow
-      v-for="feature in features"
-      :key="feature.title"
-      :feature="feature"
-    />
+    <PricingTableRow v-for="feature in features" :key="feature.title" :feature="feature" />
   </div>
 </template>
 
