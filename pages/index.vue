cc3214bca0031098ee3eb4127719bda1b66c872e
Bram <bram@owow.io>
2021-04-06 10:20:23 +0200

    Deleted unnecessary code in the index

--- pages/index.vue
+++ pages/index.vue
@@ -6,10 +6,6 @@
     <FeatureSection />
     <PricingTableSection />
     <FaqSection />
-    <div class="grid lg:grid-cols-3 gap-6 2xl:gap-y-12 md:grid-cols-2 sm:grid-cols-1">
-      <FeatureTile v-for="feature in features" :key="feature.headline" :feature="feature" />
-    </div>
-
     <CtaSection />
   </div>
 </template>
