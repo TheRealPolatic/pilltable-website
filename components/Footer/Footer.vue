3661cb9af6e4e5f11dc0ddbd462a8dc112dcd63d
mdw_08 <michelledewit1.mdw@gmail.com>
2021-04-06 11:54:21 +0200

    Footer emojis + responsive mobile added

--- components/Footer/Footer.vue
+++ components/Footer/Footer.vue
@@ -1,15 +1,26 @@
 <template>
   <footer class="">
-    <div class="max-w-7xl mx-auto py-12 px-4 sm:px-6 md:flex md:items-center md:justify-between lg:px-8">
-      <div class="mt-8 md:mt-0 flex justify-center space-x-6">
-        <a href="#" class="text-center text-base text-white hover:underline">contact@pilltable.io</a>
-        <a href="#" class="text-center text-base text-white hover:underline">Instagram</a>
-        <a href="#" class="text-center text-base text-white hover:underline">Facebook</a>
+    <div class="mx-auto py-12 px-6 md:flex md:items-center md:justify-between">
+      <div class="md:flex md:justify-center md:space-x-16">
+        <a href="#" class="flex flex-row">
+          <img class="h-10 md:ml-0 -ml-2.5" :src="require(`~/assets/emojis/mail.png`)" />
+          <p href="#" class="text-center hover:underline mt-2 text-base text-white font-medium">contact@pilltable.io</p>
+        </a>
+
+        <a href="#" class="flex flex-row">
+          <img class="h-10 -mt-0.5 md:ml-0 -ml-3" :src="require(`~/assets/emojis/rainbow.png`)" />
+          <p href="#" class="text-center hover:underline mt-2 text-base text-white font-medium">Instagram</p>
+        </a>
+
+        <a href="#" class="flex flex-row">
+          <img class="h-10 -mt-0.5 md:ml-0 -ml-3" :src="require(`~/assets/emojis/book.png`)" />
+          <p href="#" class="text-center hover:underline mt-2 text-base text-white font-medium">Facebook</p>
+        </a>
       </div>
-      <div class="flex justify-center space-x-6">
-        <p class="text-white">© Pilltable inc.</p>
-        <p class="text-white">Terms of Service</p>
-        <p class="text-white">Privacy Policy</p>
+      <div class="flex flex-col md:flex-row md:justify-center mt-4 md:mt-0 md:space-x-16">
+        <a href="#" class="md:text-center hover:underline text-base text-white font-medium">© Pilltable inc.</a>
+        <a href="#" class="md:text-center hover:underline text-base text-white font-medium md:mt-0 mt-2">Terms of Service</a>
+        <a href="#" class="md:text-center hover:underline text-base text-white font-medium md:mt-0 mt-2">Privacy Policy</a>
       </div>
     </div>
   </footer>
