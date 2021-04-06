c539fbf7b36f56ac4ac3a3a2cfd57ed8a584e26b
mdw_08 <michelledewit1.mdw@gmail.com>
2021-04-06 10:37:58 +0200

    Footer set-up added

---
+++ components/Footer/Footer.vue
@@ -0,0 +1,16 @@
+<template>
+  <footer class="">
+    <div class="max-w-7xl mx-auto py-12 px-4 sm:px-6 md:flex md:items-center md:justify-between lg:px-8">
+      <div class="mt-8 md:mt-0 flex justify-center space-x-6">
+        <a href="#" class="text-center text-base text-white hover:underline">contact@pilltable.io</a>
+        <a href="#" class="text-center text-base text-white hover:underline">Instagram</a>
+        <a href="#" class="text-center text-base text-white hover:underline">Facebook</a>
+      </div>
+      <div class="flex justify-center space-x-6">
+        <p class="text-white">© Pilltable inc.</p>
+        <p class="text-white">Terms of Service</p>
+        <p class="text-white">Privacy Policy</p>
+      </div>
+    </div>
+  </footer>
+</template>
